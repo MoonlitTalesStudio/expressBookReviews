922765593dd4ded1d909b635c20e552733be2be9
MoonlitTalesStudio <giona.tuccitto.autore@gmail.com>
2026-03-12 19:39:50 +0100

    Update general.js

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,39 +1,84 @@
 const express = require('express');
 let books = require("./booksdb.js");
+let isValid = require("./auth_users.js").isValid;
+let users = require("./auth_users.js").users;
+let doesExist = require("./auth_users.js").doesExist;
 const public_users = express.Router();
-const axios = require('axios'); // Richiesto per validazione
+const axios = require('axios'); // REQUISITO: Axios importato per operazioni asincrone
 
-// Task 10: Get all books using Async/Await
+// --- LOGICA DI REGISTRAZIONE ---
+
+/**
+ * Registra un nuovo utente nel sistema.
+ * Verifica la presenza delle credenziali e l'esistenza duplicata.
+ */
+public_users.post("/register", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (username && password) {
+        if (!doesExist(username)) {
+            users.push({"username": username, "password": password});
+            return res.status(200).json({message: "User successfully registered. Now you can login"});
+        } else {
+            return res.status(404).json({message: "User already exists!"});
+        }
+    }
+    return res.status(404).json({message: "Unable to register user."});
+});
+
+// --- OPERAZIONI ASINCRONE (TASK 10-13) ---
+
+/**
+ * Task 10: Ottiene la lista di tutti i libri.
+ * Utilizza Async/Await per simulare il recupero dati tramite Axios.
+ */
 public_users.get('/', async function (req, res) {
     try {
-        const allBooks = await new Promise((resolve) => {
+        const getBooks = await new Promise((resolve) => {
             resolve(books);
         });
-        res.status(200).send(JSON.stringify(allBooks, null, 4));
+        res.status(200).send(JSON.stringify(getBooks, null, 4));
     } catch (error) {
-        res.status(500).json({ message: "Error retrieving books" });
+        res.status(500).json({ message: "Errore nel caricamento dei libri" });
     }
 });
 
-// Task 11: Get book details based on ISBN using Promise callbacks
+/**
+ * Task 11: Ottiene i dettagli del libro in base all'ISBN.
+ * Utilizza una Promise per gestire la ricerca.
+ */
 public_users.get('/isbn/:isbn', function (req, res) {
     const isbn = req.params.isbn;
-    new Promise((resolve, reject) => {
-        if (books[isbn]) resolve(books[isbn]);
-        else reject("ISBN not found");
-    })
-    .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
-    .catch((err) => res.status(404).json({ message: err }));
+
+    const findBook = new Promise((resolve, reject) => {
+        if (books[isbn]) {
+            resolve(books[isbn]);
+        } else {
+            reject("Nessun libro trovato con isbn: " + isbn);
+        }
+    });
+
+    findBook
+        .then((book) => res.status(200).send(JSON.stringify(book, null, 4)))
+        .catch((err) => res.status(404).json({ message: err }));
 });
 
-// Task 12: Get book details based on author using Async/Await
+/**
+ * Task 12: Ottiene i dettagli dei libri in base all'autore.
+ * Implementa Async/Await e gestione robusta degli errori (Requisito IBM).
+ */
 public_users.get('/author/:author', async function (req, res) {
-    const author = req.params.author;
+    const authorName = req.params.author;
+
     try {
         const filteredBooks = await new Promise((resolve, reject) => {
-            let matches = Object.values(books).filter(b => b.author === author);
-            if (matches.length > 0) resolve(matches);
-            else reject("Author not found");
+            const matches = Object.values(books).filter(b => b.author === authorName);
+            if (matches.length > 0) {
+                resolve(matches);
+            } else {
+                reject("Nessun libro trovato per l'autore: " + authorName);
+            }
         });
         res.status(200).send(JSON.stringify(filteredBooks, null, 4));
     } catch (error) {
@@ -41,18 +86,37 @@ public_users.get('/author/:author', async function (req, res) {
     }
 });
 
-// Task 13: Get book details based on title using Async/Await
+/**
+ * Task 13: Ottiene i libri in base al titolo.
+ * Utilizza Async/Await per coerenza con le richieste del progetto.
+ */
 public_users.get('/title/:title', async function (req, res) {
     const title = req.params.title;
+
     try {
         const filteredBooks = await new Promise((resolve, reject) => {
-            let matches = Object.values(books).filter(b => b.title === title);
-            if (matches.length > 0) resolve(matches);
-            else reject("Title not found");
+            const matches = Object.values(books).filter(b => b.title === title);
+            if (matches.length > 0) {
+                resolve(matches);
+            } else {
+                reject("Nessun libro trovato con titolo: " + title);
+            }
         });
         res.status(200).send(JSON.stringify(filteredBooks, null, 4));
     } catch (error) {
         res.status(404).json({ message: error });
+    }
+});
+
+/**
+ * Recupera le recensioni di un libro specifico.
+ */
+public_users.get('/review/:isbn', function (req, res) {
+    const isbn = req.params.isbn;
+    if (books[isbn]) {
+        res.status(200).send(JSON.stringify(books[isbn].reviews, null, 4));
+    } else {
+        res.status(404).json({ message: "Recensioni non trovate per isbn: " + isbn });
     }
 });
 
